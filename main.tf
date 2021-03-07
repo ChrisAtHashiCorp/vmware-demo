db01047c45071aced55affd4d934589a64d6f253
Chris Polansky <cpolansky@hashicorp.com>
2021-03-07 21:04:44 +0000

    Small changes

--- main.tf
+++ main.tf
@@ -1,27 +1,3 @@
-data "vsphere_datacenter" "dc" {
-  name = var.dc_name
-}
-
-data "vsphere_datastore" "datastore" {
-  name          = var.datastore_name
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_resource_pool" "pool" {
-  name          = var.resource_pool_name
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_network" "network" {
-  name          = var.vm_network_name
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_virtual_machine" "template" {
-  name          = "Templates/centos8-template"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
 resource "vsphere_virtual_machine" "test-vm" {
   name             = "cpolansky-test"
   resource_pool_id = data.vsphere_resource_pool.pool.id
@@ -47,14 +23,5 @@ resource "vsphere_virtual_machine" "test-vm" {
 
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
-
-  #  customize {
-  #    linux_options {
-  #      host_name = "cpolansky-test"
-  #      domain    = "hashicorp.com"
-  #    }
-
-  #    network_interface {}
-  # }
   }
 }
